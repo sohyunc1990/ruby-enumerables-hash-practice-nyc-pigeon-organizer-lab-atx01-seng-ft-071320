dc48f0a90d24fdca2943f2d6cec285e3cbd6a00d
Sohyun Lee <sohyunc1990@gmail.com>
2020-07-06 19:27:41 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,10 +1,9 @@
-require 'pry'
+
 def nyc_pigeon_organizer(data)
   # write your code here!
 final_results = data.each_with_object({}) do |(key, value), final_array|
     value.each do |inner_key, names|
       names.each do |name|
-        binding.pry
         if !final_array[name]
         final_array[name]={}
         end
